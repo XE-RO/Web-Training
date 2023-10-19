3edb5c6cf26702a947f1fb8ab4cb35a56b1d9a11
XE-RO <delgadox0@gmail.com>
2023-10-19 08:58:32 -0600

    Countdown

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock,alarm } from "./reloj.js";
 import { moveBall,shortCuts } from "./teclado.js";
+import countdown from "./countdown.js";
 
 const d=document
 
@@ -8,6 +9,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj","#reset-reloj");
     alarm("./assets/iPhone-15-Plus.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown","Jan 03, 2024","Feliz Cumpleaños")
 })
 
 d.addEventListener("keydown",e=>{
